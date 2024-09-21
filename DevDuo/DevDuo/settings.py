0693c16b16c96ec4424061a745f39454c1a5c3d9
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 23:30:34 +0200

    Added key

--- DevDuo/DevDuo/settings.py
+++ DevDuo/DevDuo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('django-insecure-^l78)1+6y2u#r%sm6v2+^$rtxukt^c6e_rkfh*e(0c=skmk)gx')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
