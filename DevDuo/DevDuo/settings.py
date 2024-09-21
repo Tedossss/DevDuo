d3463f4ee907fd9bfd33e4236e20a6b8f2443319
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 23:46:33 +0200

    added old static files

--- DevDuo/DevDuo/settings.py
+++ DevDuo/DevDuo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
 from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     BASE_DIR / "main/static",
