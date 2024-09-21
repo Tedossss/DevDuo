2adc21c85a7e091becf33b9a3bb9bb2b09069489
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 04:18:49 +0200

    add all

--- webit/webit/settings.py
+++ webit/webit/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^l78)1+6y2u#r%sm6v2+^$rtxukt^c6e_rkfh*e(0c=skmk)gx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
