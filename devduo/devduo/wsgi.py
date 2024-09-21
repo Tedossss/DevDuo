3c5890e5d533eed56b8327f8a5c1dc64eacbd0f9
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 22:35:36 +0200

    Fix project name

--- devduo/devduo/wsgi.py
+++ devduo/devduo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for webit project.
+WSGI config for devduo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'devduo.settings')
 
 application = get_wsgi_application()
