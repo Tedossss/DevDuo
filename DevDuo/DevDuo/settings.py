c207c021672aa3f09621ad5ac04be75b078a228c
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 23:53:57 +0200

    idk dirs

--- DevDuo/DevDuo/settings.py
+++ DevDuo/DevDuo/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "main/static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
