f73d09bd63b9ec01658e9cde572da1a1f643c0c0
Tedossss <Nazarfalach51@gmail.com>
2024-09-21 23:19:41 +0200

    Added ALLOWED_HOSTS

--- DevDuo/DevDuo/settings.py
+++ DevDuo/DevDuo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 from django.conf.global_settings import STATICFILES_DIRS
 
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^l78)1+6y2u#r%sm6v2+^$rtxukt^c6e_rkfh*e(0c=skmk)gx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devduo.onrender.com']
 
 
 # Application definition
